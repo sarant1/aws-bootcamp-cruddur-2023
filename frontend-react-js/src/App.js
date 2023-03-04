da4ec3c8cdea6bcb55d226a2361ba2df1350c33d
Samuel Arant <samuel.arant10@gmail.com>
2023-03-04 18:09:54 +0000

    integrate cognito

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -16,7 +16,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 Amplify.configure({
   "AWS_PROJECT_REGION": process.env.REACT_APP_AWS_PROJECT_REGION,
-  "aws_cognito_identity_pool_id": process.env.REACT_APP_AWS_COGNITO_IDENTITY_POOL_ID,
   "aws_cognito_region": process.env.REACT_APP_AWS_COGNITO_REGION,
   "aws_user_pools_id": process.env.REACT_APP_AWS_USER_POOLS_ID,
   "aws_user_pools_web_client_id": process.env.REACT_APP_CLIENT_ID,
