e86c26cb543ca455105a7de5469e871ac2e721eb
Samuel Arant <samuel.arant10@gmail.com>
2023-03-05 01:03:14 +0000

    correctly instrumented xray and subsegments

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -44,8 +44,8 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 # LOGGER.info("test log")
 
 # X-RAY ------
-#xray_url = os.getenv("AWS_XRAY_URL")
-#xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 
 # HoneyComb -------
 # Initialize automatic instrumentation with Flask
@@ -61,7 +61,7 @@ tracer = trace.get_tracer(__name__)
 app = Flask(__name__)
 
 # X-Ray -----
-# XRayMiddleware(app, xray_recorder)
+XRayMiddleware(app, xray_recorder)
 
 # HoneyComb -------
 # Initialize automatic instrumentation with Flask
@@ -79,7 +79,7 @@ cors = CORS(
   methods="OPTIONS,GET,HEAD,POST"
 )
 
-# Cloudwatch logs
+# Cloudwatch Logs -----
 # @app.after_request
 # def after_request(response):
 #     timestamp = strftime('[%Y-%b-%d %H:%M]')
@@ -150,11 +150,13 @@ def data_notifications():
   return data, 200
 
 @app.route("/api/activities/home", methods=['GET'])
+@xray_recorder.capture('activities_home')
 def data_home():
   data = HomeActivities.run()
   return data, 200
 
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
+@xray_recorder.capture('activites_users')
 def data_handle(handle):
   model = UserActivities.run(handle)
   if model['errors'] is not None:
@@ -186,6 +188,7 @@ def data_activities():
   return
 
 @app.route("/api/activities/<string:activity_uuid>", methods=['GET'])
+@xray_recorder.capture('activites_show')
 def data_show_activity(activity_uuid):
   data = ShowActivity.run(activity_uuid=activity_uuid)
   return data, 200
--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -3,35 +3,38 @@ from aws_xray_sdk.core import xray_recorder
 
 class UserActivities:
   def run(user_handle):
+    try:
+      # x-ray -----
+      segment = xray_recorder.begin_segment('user_activities')
+      model = {
+        'errors': None,
+        'data': None
+      }
 
-    #x-ray -----
-    #segment = xray_recorder.begin_segment('user_activities')
-    model = {
-      'errors': None,
-      'data': None
-    }
-
-    #x-rayy
-    now = datetime.now(timezone.utc).astimezone()
+      #x-rayy
+      now = datetime.now(timezone.utc).astimezone()
+      
     
-   
-    if user_handle == None or len(user_handle) < 1:
-      model['errors'] = ['blank_user_handle']
-    else:
-      now = datetime.now()
-      results = [{
-        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-        'handle':  'Andrew Brown',
-        'message': 'Cloud is fun!',
-        'created_at': (now - timedelta(days=1)).isoformat(),
-        'expires_at': (now + timedelta(days=31)).isoformat()
-      }]
-      model['data'] = results
-    #subsegment = xray_recorder.begin_subsegment('mock-data')
-    # x-ray ----
-    # dict = {
-    #   "now": now.isoformat(),
-    #   "results-size": len(model['data'])
-    # }
-    # subsegment.put_metadata('key', dict, 'namespace')
+      if user_handle == None or len(user_handle) < 1:
+        model['errors'] = ['blank_user_handle']
+      else:
+        now = datetime.now()
+        results = [{
+          'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+          'handle':  'Andrew Brown',
+          'message': 'Cloud is fun!',
+          'created_at': (now - timedelta(days=1)).isoformat(),
+          'expires_at': (now + timedelta(days=31)).isoformat()
+        }]
+        model['data'] = results
+      subsegment = xray_recorder.begin_subsegment('mock-data')
+      # x-ray ----
+      dict = {
+        "now": now.isoformat(),
+        "results-size": len(model['data'])
+      }
+      subsegment.put_metadata('key', dict, 'namespace')
+    finally:
+      # close the subsegment
+      xray_recorder.end_subsegment()
     return model
