55c1f1fd99f3ce15c155cd3c697c0b27499eee39
sarant1 <samuel.arant10@gmail.com>
2023-03-21 22:24:42 +0000

    added ddb utilities, imporve print in db.py, finished ddb utils

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -16,11 +16,11 @@ class Db:
     print(f'{blue}SQL-PARAMS-----------------{no_color}')
     print(params)
 
-  def print_sql(self, title, sql):
-    cyan = "\033[96m"
-    no_color = "\033[0m"
-    print(f'{cyan}SQL STATMENT-[{title}]------{no_color}')
-    print(sql + "\n")
+  def print_sql(self,title,sql,params={}):
+    cyan = '\033[96m'
+    no_color = '\033[0m'
+    print(f'{cyan} SQL STATEMENT-[{title}]------{no_color}')
+    print(sql,params)
 
   def template(self, *args):
     pathing = list((app.root_path, 'db', 'sql') + args)
@@ -35,7 +35,7 @@ class Db:
 
   # be sure to check for RETURNING in all uppercases
   def query_commit(self, sql, params={}):
-    self.print_sql('commit with returning', sql)
+    self.print_sql('commit with returning', sql, params)
     
     pattern = r"\bRETURNING\b"
     is_returning_id = re.search(pattern, sql)
@@ -56,7 +56,7 @@ class Db:
 
   # When we want to return a json object
   def query_array_json(self, sql, params={}):
-    self.print_sql('array', sql)
+    self.print_sql('array', sql, params)
     print("SQL STATEMENT-------[array]----------")
     print(sql)
     print("SQL STATEMENT------------------")
@@ -72,7 +72,7 @@ class Db:
 
   # When we want to return an array of json objects
   def query_object_json(self, sql, params={}):
-    self.print_sql('json', sql)
+    self.print_sql('json', sql, params)
 
     wrapped_sql = self.query_wrap_object(sql)
     self.print_params(params)
@@ -87,6 +87,14 @@ class Db:
         cur.execute(wrapped_sql, params)
         # this will return a tuple
         # the first field being the data
+        json = cur.fetchone()
+        return json[0]
+  
+  def query_value(self, sql, params={}):
+    self.print_sql('value', sql, params)
+    with self.pool.connection() as conn:
+      with conn.cursor() as cur:
+        cur.execute(sql,params)
         json = cur.fetchone()
         return json[0]
 
