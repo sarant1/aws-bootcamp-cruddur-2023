0bd2ce70250cc8ca9fcb79e173a00d123a14b2b9
sarant1 <samuel.arant10@gmail.com>
2023-03-21 05:09:04 +0000

    still debugging local environment

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -8,8 +8,6 @@ import ActivityForm from '../components/ActivityForm';
 import ReplyForm from '../components/ReplyForm';
 import { Auth } from 'aws-amplify';
 
-// [TODO] Authenication
-import Cookies from 'js-cookie'
 
 export default function HomeFeedPage() {
   const [activities, setActivities] = React.useState([]);
@@ -23,14 +21,15 @@ export default function HomeFeedPage() {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`
       const res = await fetch(backend_url, {
+        mode: 'no-cors',
         headers: {
           Authorization: `Bearer ${localStorage.getItem("access_token")}`
         },
         method: "GET"
       });
-      let resJson = await res.json();
+      
       if (res.status === 200) {
-        setActivities(resJson)
+        setActivities(res)
       } else {
         console.log(res)
       }
