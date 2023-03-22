5f84151ba2ca7e6760a682ab8d54e18fda8afa59
sarant1 <samuel.arant10@gmail.com>
2023-03-22 02:18:27 +0000

    added update cognito script and list users script

---
+++ backend-flask/lib/ddb.py
@@ -0,0 +1,27 @@
+import os,sys
+import boto3
+from datetime import datetime, timedelta, timezone
+import uuid
+
+class Ddb:
+    def client():
+        endpoint_url = os.getenv("AWS_ENDPOINT_URL")
+        if endpoint_url:
+            attrs = { 'endpoint_url': endpoint_url }
+        else:
+            attrs = {}
+        dynamodb = boto3.client('dynamodb',**attrs)
+        return dynamodb
+    def list_message_groups(client,my_user_uuid):
+        current_year = datetime.now().year
+        table_name = 'cruddur-message'
+        query_params = {
+        'TableName': table_name,
+        'KeyConditionExpression': 'pk = :pk AND begins_with(sk,:year)',
+        'ScanIndexForward': False,
+        'Limit': 20,
+        'ExpressionAttributeValues': {
+            ':year': {'S': str(current_year) },
+            ':pk': {'S': f"GRP#{my_user_uuid}"}
+            }
+        }
