aaecb763076d7be98f2d85e9fa6abbd7c3a94933
sarant1 <samuel.arant10@gmail.com>
2023-03-30 02:15:00 +0000

    hard coded connect to functions, added front-end task function

--- frontend-react-js/src/components/DesktopNavigationLink.js
+++ frontend-react-js/src/components/DesktopNavigationLink.js
@@ -18,19 +18,14 @@ export default function DesktopNavigationLink(props) {
     switch(props.handle){
       case 'home':
         return <HomeIcon className='icon' />
-        break;
       case 'notifications':
         return <NotificationsIcon className='icon' />
-        break;
       case 'profile':
         return <ProfileIcon className='icon' />
-        break;
       case 'more':
         return <MoreIcon className='icon' />
-        break;
       case 'messages':
         return <MessagesIcon className='icon' />
-        break;
     }
   }
 
--- journal/week6.md
+++ journal/week6.md
@@ -26,3 +26,5 @@
 fixed: I added the permissions to the exection role and it worked (b4 I was adding to the task role)
 
 - I used the Load Balancer ARN instead of the target group ARN which was giving me a permisisons error
+
+- I am using a load balancer currrently so I had to configure it so that if the host header is from my IONOS domain routes to port 444 which is cruddur
