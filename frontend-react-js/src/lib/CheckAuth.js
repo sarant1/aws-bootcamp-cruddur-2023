7ed92986cb88382e640f0507087a5e6132cf8eb3
sarant1 <samuel.arant10@gmail.com>
2023-04-02 22:39:10 +0000

    fixed auth so it doesn't timeout

--- frontend-react-js/src/lib/CheckAuth.js
+++ frontend-react-js/src/lib/CheckAuth.js
@@ -1,22 +1,36 @@
 import { Auth } from 'aws-amplify';
 
-const checkAuth = async (setUser) => {
+
+export const getAccessToken = async () => {
+  try {
+    Auth.currentSession()
+    .then((cognito_user) => {
+      const access_token = cognito_user.idToken.jwtToken
+      localStorage.setItem('access_token', access_token)
+      return access_token
+    })
+  } catch (err) {
+    console.log(err);
+    return null
+  }
+}
+export const checkAuth = async (setUser) => {
     Auth.currentAuthenticatedUser({
       // Optional, By default is false. 
       // If set to true, this call will send a 
       // request to Cognito to get the latest user data
       bypassCache: false 
     })
-    .then((user) => {
-      console.log('user',user);
-      return Auth.currentAuthenticatedUser()
-    }).then((cognito_user) => {
-        setUser({
-          display_name: cognito_user.attributes.name,
-          handle: cognito_user.attributes.preferred_username
-        })
+    .then((cognito_user) => {
+      console.log('cognito_user', cognito_user);
+      setUser({
+        display_name: cognito_user.attributes.name,
+        handle: cognito_user.attributes.preferred_username
+      })
+      return Auth.currentSession()
+    }).then((cognito_user_session) => {
+      console.log('cognito_user_session',cognito_user_session)
+      localStorage.setItem("access_token", cognito_user_session.accessToken.jwtToken)
     })
     .catch((err) => console.log(err));
   };
-
-  export default checkAuth;
--- frontend-react-js/src/pages/MessageGroupPage.js
+++ frontend-react-js/src/pages/MessageGroupPage.js
@@ -6,7 +6,7 @@ import DesktopNavigation  from '../components/DesktopNavigation';
 import MessageGroupFeed from '../components/MessageGroupFeed';
 import MessagesFeed from '../components/MessageFeed';
 import MessagesForm from '../components/MessageForm';
-import checkAuth from '../lib/CheckAuth';
+import {checkAuth, getAccessToken} from '../lib/CheckAuth';
 
 
 export default function MessageGroupPage() {
@@ -20,9 +20,10 @@ export default function MessageGroupPage() {
   const loadMessageGroupsData = async () => {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/message_groups`
+      const access_token = await getAccessToken();
       const res = await fetch(backend_url, {
         headers: {
-          Authorization: `Bearer ${localStorage.getItem("access_token")}`
+          Authorization: `Bearer ${access_token}`
         },
         method: "GET"
       });
@@ -40,9 +41,10 @@ export default function MessageGroupPage() {
   const loadMessageGroupData = async () => {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/messages/${params.message_group_uuid}`
+      const access_token = await getAccessToken();
       const res = await fetch(backend_url, {
         headers: {
-          Authorization: `Bearer ${localStorage.getItem("access_token")}`
+          Authorization: `Bearer ${access_token}`
         },
         method: "GET"
       });
