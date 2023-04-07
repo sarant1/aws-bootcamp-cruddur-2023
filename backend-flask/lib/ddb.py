ddba9bd58b5887b2355f222832beee328ff18db9
sarant1 <samuel.arant10@gmail.com>
2023-04-07 17:47:48 +0000

    added missing () to create_message function

--- backend-flask/lib/ddb.py
+++ backend-flask/lib/ddb.py
@@ -78,7 +78,7 @@ class Ddb:
         return results
     
     def create_message(client,message_group_uuid, message, my_user_uuid, my_user_display_name, my_user_handle):
-        created_at = datetime.now().isoformat
+        created_at = datetime.now().isoformat()
         message_uuid = str(uuid.uuid4())
 
         record = {
