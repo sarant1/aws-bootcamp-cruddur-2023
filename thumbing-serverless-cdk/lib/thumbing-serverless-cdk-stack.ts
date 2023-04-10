f7f4de44e26a262b2b75a413a73b672147a96a8f
sarant1 <samuel.arant10@gmail.com>
2023-04-10 17:32:00 +0000

    1/2 implement image process cdk

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -3,10 +3,10 @@ import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as dotenv from 'dotenv';
+import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
 
 // Load env variables
 dotenv.config();
-
 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -16,22 +16,38 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-    const bucketName: string = process.env.THUMBING_BUCKET as string;
-    const functionPath: string = process.env.THUMBING_FUNCTION_PATH as string;
+    const bucketName: string = process.env.THUMBING_BUCKET_NAME as string;
     const folderInput: string = process.env.THUMBING_S3_FOLDER_INPUT as string;
     const folderOutput: string = process.env.THUMBING_S3_FOLDER_OUTPUT as string;
+    const webhookUrl: string = process.env.THUMBING_WEBHOOK_URL as string;
+    const topicName: string = process.env.THUMBING_TOPIC_NAME as string;
+    const functionPath: string = process.env.THUMBING_FUNCTION_PATH as string;
+    console.log('bucketName',bucketName)
+    console.log('folderInput',folderInput)
+    console.log('folderOutput',folderOutput)
+    console.log('webhookUrl',webhookUrl)
+    console.log('topicName',topicName)
+    console.log('functionPath',functionPath)
 
 
-    const bucket = this.createBucket(bucketName);
+    //const bucket = this.createBucket(bucketName);
+    const bucket = this.importBucket(bucketName);
     const lambda = this.createLambda(functionPath, bucketName, folderInput, folderOutput);
+
+    this.createS3NotifyToLambda(folderInput,lambda,bucket)
   }
 
 
   createBucket(bucketName: string): s3.IBucket {
-    const bucket = new s3.Bucket(this, 'ThumbingBucket', {
+    const bucket = new s3.Bucket(this, 'AssetsBucket', {
       bucketName: bucketName,
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
+    return bucket;
+  }
+
+  importBucket(bucketName: string): s3.IBucket {
+    const bucket = s3.Bucket.fromBucketName(this, 'AssetsBucket', bucketName);
     return bucket;
   }
 
@@ -49,5 +65,14 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
       }
     });
     return lambdaFunction;
+  }
+
+  createS3NotifyToLambda(prefix: string, lambda: lambda.IFunction, bucket: s3.IBucket): void {
+    const destination = new s3n.LambdaDestination(lambda);
+    bucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED_PUT,
+      destination,
+      {prefix: prefix} // folder to contain original folder image
+    )
   }
 }
