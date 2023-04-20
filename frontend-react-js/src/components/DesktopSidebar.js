af8808b2f296fc17ecf28883eb141fc7651a2882
sarant1 <samuel.arant10@gmail.com>
2023-04-20 16:11:08 +0000

    made it go to my profile url instead of /andrewrbown

--- frontend-react-js/src/components/DesktopSidebar.js
+++ frontend-react-js/src/components/DesktopSidebar.js
@@ -13,7 +13,7 @@ export default function DesktopSidebar(props) {
   ]
 
   const users = [
-    {"display_name": "Andrew Brown", "handle": "andrewbrown"}
+    {"display_name": "Andrew Brown", "handle": "samuelarant"}
   ]
 
   let trending;
